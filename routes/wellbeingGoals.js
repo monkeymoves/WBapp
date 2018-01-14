dd2d5c1b83ef3b1bbb794971c51cc4462029f5fe
luke maggs <lukejjmaggs@gmail.com>
2018-01-14 16:16:56 +0000

    export PDF

--- routes/wellbeingGoals.js
+++ routes/wellbeingGoals.js
@@ -5,41 +5,41 @@ var WBprosperous = {
 }
 
 var WBresilient = {
-    name: "A resilient Wales ",
+    name: "A Resilient Wales ",
     content: "A nation which maintains and enhances a biodiverse natural environment with healthy functioning ecosystems that support social, economic and ecological resilience and the capacity to adapt to change (for example climate change).",
     url:"/resilient",
 
 }
 
 var WBhealthier = {
-    name: "A healthier Wales ",
+    name: "A Healthier Wales ",
     content: "A society in which people’s physical and mental well-being is maximised and in which choices and behaviours that benefit future health are understood.",
     url:"/healthier",
 
 }
 
 var WBequal = {
-    name: "A more equal Wales ",
+    name: "An Equal Wales ",
     content: "A society that enables people to fulfil their potential no matter what their background or circumstances (including their socio economic background and circumstances).",
     url:"/equal",
 
 }
 
 var WBcohesive = {
-    name: "A Wales of cohesive communities",
+    name: "A Cohesive Wales",
     content: "Attractive, viable, safe and well-connected communities.",
     url:"/cohesive",
 
 }
 
 var WBvibrant = {
-    name: "A Wales of vibrant  culture and thriving Welsh language",
+    name: "A Vibrant & Cultural Wales",
     content: "A society that promotes and protects culture, heritage and the Welsh language, and which encourages people to participate in the arts, and sports and recreation.",
     url:"/vibrant",
 
 }
 var WBglobal = {
-    name: "A globally responsible Wales",
+    name: "A Global Wales",
     content: "A globally responsible Wales. A nation which, when doing anything to improve the economic, social, environmental and cultural well-being of Wales, takes account of whether doing such a thing may make a positive contribution to global well-being and the capacity to adapt to change (for example climate change).",
     url:"/global",
 
